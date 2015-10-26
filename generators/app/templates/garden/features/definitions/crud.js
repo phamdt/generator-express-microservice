98b26ab967bb886ac6e2f74a84409ed7cebe3875
danny pham <pham.danny.t@gmail.com>
2015-10-26 07:09:32 -0700

    test directory change and tweaks

--- generators/app/templates/garden/features/definitions/crud.js
+++ generators/app/templates/garden/features/definitions/crud.js
@@ -10,9 +10,11 @@ module.exports = function() {
     this.api.addHeaders(header, callback);
   });
 
-  this.When('requesting all <% modelName %>s', function(callback) {
+  this.When('requesting all <%= modelName %>s', function(callback) {
     var self = this;
-    this.api.get(self.indexUrl).then(callback);
+    this.api
+      .get(self.indexUrl)
+      .then(callback);
   });
 
   this.Then('the service should return a list of the user\'s <%= modelName %>s', function(callback) {

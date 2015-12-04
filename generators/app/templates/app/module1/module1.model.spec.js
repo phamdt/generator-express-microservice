1fd589e0e4f42604363e1be8b34f5f8f5952cb34
danny pham <pham.danny.t@gmail.com>
2015-12-03 17:45:00 -0800

    adds empty model test and some comments

---
+++ generators/app/templates/app/module1/module1.model.spec.js
@@ -0,0 +1,33 @@
+describe('<%= modelName %>', () => {
+  let thing = null;
+  const otherthing = 'abcdefg';
+
+  beforeEach( done => {
+
+    done();
+  });
+
+  describe('a model behavior', () => {
+    beforeEach( done => {
+
+    });
+
+    it('should behave as expected in a specific way', (done) => {
+      done();
+    });
+  });
+
+  describe('another model behavior', () => {
+    beforeEach( done => {
+
+    });
+
+    it('should behave as expected in a specific way', (done) => {
+      done();
+    });
+  });
+
+  function helperFunction() {
+
+  }
+});
--- generators/app/templates/middleware/index.js
+++ generators/app/templates/middleware/index.js
@@ -1,2 +1,3 @@
+// application-wide middleware
 export { default as middleware1 } from './app.middleware1';
 export { default as middleware2 } from  './app.middleware2';
